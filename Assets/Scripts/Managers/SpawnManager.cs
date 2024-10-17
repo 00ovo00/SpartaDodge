c76c797b84af41cb5e7869213cd57c0dfa67f4a2
ljj <sumkol123@gmail.com>
2024-10-17 11:58:05 +0900

    [Update]Spawn매니저 수정중

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -9,9 +9,11 @@ public class SpawnManager : MonoBehaviour
 {
     private GameObject[] spawnPointArray;
     private ObjectPool objectPool;
-    [SerializeField] private float spawnTime;
+    [SerializeField] private float spawnTime = 2;
     private string[] poolNameArray;
-    private System.Random random;
+    private float lastSpawnTime = 0f;
+
+   
     [SerializeField] private GameObject bat;
 
     private void Awake()
@@ -21,15 +23,44 @@ public class SpawnManager : MonoBehaviour
         spawnPointArray = GameObject.FindGameObjectsWithTag("SpawnPoint");
         objectPool.CreatePool("Bat", bat, 20);
         poolNameArray = new string[objectPool.Pools.Count];
-
         for (int i = 0; i < objectPool.Pools.Count; i++)
         {
             poolNameArray[i] = objectPool.Pools[i].tag;
             Debug.Log(poolNameArray[i]);
 
         }
+    
+    }
 
+    private void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        lastSpawnTime += Time.deltaTime;
+
+        if (lastSpawnTime < spawnTime) return;
+        
+        int randomIndex = Random.Range(0, spawnPointArray.Length);
+
+        lastSpawnTime = 0f;
        
+       
+    }
+
+    private void SpawnHandlerByKillCount()
+    {
+
+
+    }
+
+    public string RandomSpawnPrefabs()
+    {
+        int randomIndex = Random.Range(0, poolNameArray.Length);
+        return poolNameArray[randomIndex];
+
     }
 
 }
