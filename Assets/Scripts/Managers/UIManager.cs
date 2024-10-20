a6124650ca1823052d5756e1660da09c45ba6ffd
Yggfyhe <alstjqdl1@naver.com>
2024-10-20 19:04:55 +0900

    [Add] 화면전환 추가

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
+
+    public RectTransform maskImage;
+    public float maskSpeed = 5f;
+
+    public Button btn;
+    public CanvasGroup canvasGroup;
+
+    private Vector2 targetSize = Vector2.zero;
+    private Vector2 initialSize;
+
+    private void Start()
+    {
+        initialSize = maskImage.sizeDelta;
+    }
 
     private void Awake()
     {
@@ -16,7 +31,21 @@ public class UIManager : MonoBehaviour
     }
     public void OnStartButtonClick()
     {
-        string sceneName = "TestSceneHSH";
+        if (canvasGroup != null)
+        {
+            canvasGroup.alpha = 0f; 
+            canvasGroup.interactable = false; 
+            canvasGroup.blocksRaycasts = false; 
+        }
+        StartCoroutine(maskLoadScene("TestSceneHSH")); 
+    }
+    private IEnumerator maskLoadScene(string sceneName)
+    {
+        while (maskImage.sizeDelta != targetSize)
+        {
+            maskImage.sizeDelta = Vector2.Lerp(maskImage.sizeDelta, targetSize, Time.deltaTime * maskSpeed);
+            yield return null;
+        }
         //SceneManager.LoadScene("SampleScene");
         SceneManager.LoadScene(sceneName);
     }
